8e32e025df94e408ab3d4fb3b8f1d858bc3b4ac9
siddharth200119 <siddramnani@gmail.com>
2024-10-13 19:17:27 +0530

    added projects

--- src/Components/LandingPage/ExperienceCard.jsx
+++ src/Components/LandingPage/ExperienceCard.jsx
@@ -7,8 +7,8 @@ function ExperienceCard(props){
     return(
         <div className="flex flex-row mb-2">
             <div className="flex flex-col items-center justify-center mr-2">
-                <span className="min-h-3 min-w-3 bg-accent rounded-full"></span>
-                <span className={`flex-grow w-1 mt-0.5 min-h-5 bg-accent`}></span>
+                <span className="min-h-3 min-w-3 bg-foreground rounded-full"></span>
+                <span className={`flex-grow w-1 mt-1 min-h-5 bg-foreground`}></span>
             </div>
             <div className={`flex flex-col bg-card py-3 px-2 rounded-md mt-1 w-72 md:w-96 ${showLine?'mb-2':""} shadow-xl`}>
                 <div className="text-secondary-foreground text-sm mb-1">{data.timestamps}</div>
--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,25 +1,25 @@
 const projects = [
     {
-        title: "Make My Resume",
-        live_demo: "pending",
-        desc: "",
-        img: "",
+        title: "Orange.com",
+        live_demo: "https://orange-nhut.onrender.com/",
+        desc: "A clone of the apple website.",
+        img: "/assets/images/orange.png",
         duration: "",
-        tools: ["Javascript", "NodeJS", "ReactJS", "MongoDB", "GEMINI"]
+        tools: ["Javascript", "NodeJS", "MongoDB", "EJS"]
     },
     {
         title: "Smart Gaze",
-        live_demo: "insert YT video",
-        desc: "",
+        live_demo: "",
+        desc: "A raspberry pi based smart mirror.",
         github: "https://github.com/siddharth200119/SmartGaze",
-        img: "",
+        img: "/assets/images/smart-gaze.jpg",
         duration: "",
         tools: ["Python", "Mediapipe", "ReactJS", "Django", "SQL", "Embedded Systems", "OpenCV"]
     },
     {
         title: "React Form Factory",
-        live_demo: "create demo on codepen or something",
-        desc: "",
+        live_demo: "https://www.npmjs.com/package/react-json-form-factory",
+        desc: "A NPM package used to build forms.",
         github: "https://github.com/siddharth200119/react-form-factory",
         img: "/assets/images/react_form_factory.png",
         npm: "https://www.npmjs.com/package/react-json-form-factory",
@@ -29,9 +29,9 @@ const projects = [
     {
         title: "Almashines Clone",
         live_demo: "https://siddharth200119.github.io/Almashines-clone/",
-        desc: "",
+        desc: "A clone of the Almashines website.",
         github: "https://github.com/siddharth200119/Almashines-clone",
-        img: "",
+        img: "/assets/images/alma-clone.png",
         duration: "",
         tools: ["HTML", "CSS", "JS"]
     }
